946e7d5a7ff48cd5ecdb725db7923c50982ae644
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-03-07 16:26:54 -0500

    Create Installation.py

---
+++ MistralUtils/Installation.py
@@ -0,0 +1,3 @@
+## LLAMA CPP PYTHON
+#!CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install /content/drive/MyDrive/llama_cpp_python-0.2.53-cp310-cp310-manylinux_2_35_x86_64.whl
+#!CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama_cpp_python
